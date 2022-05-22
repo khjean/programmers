7d090aa0ec45e55e7be05d9d6a3d926fa43115a9
김혁진 <khjean4078@naver.com>
2022-05-23 00:00:57 +0900

    프로그래머스 숫자문자열과영단어

---
+++ src/level1/숫자문자열과영단어/README.md
@@ -0,0 +1,3 @@
+### 숫자문자열과영단어
+
+[문제설명] (https://programmers.co.kr/learn/courses/30/lessons/81301)
---
+++ src/level1/숫자문자열과영단어/Solution.java
@@ -0,0 +1,57 @@
+package level1.숫자문자열과영단어;
+
+public class Solution {
+
+//    1478 → "one4seveneight"
+//    234567 → "23four5six7"
+//    10203 → "1zerotwozero3"
+    public int solution(String s) {
+        for(int i=0;i<10;i++){
+            String source="";
+            switch (i){
+                case 0:
+                    source = "zero";
+                    break;
+                case 1:
+                    source = "one";
+                    break;
+                case 2:
+                    source = "two";
+                    break;
+                case 3:
+                    source = "three";
+                    break;
+                case 4:
+                    source = "four";
+                    break;
+                case 5:
+                    source = "five";
+                    break;
+                case 6:
+                    source = "six";
+                    break;
+                case 7:
+                    source = "seven";
+                    break;
+                case 8:
+                    source = "eight";
+                    break;
+                case 9:
+                    source = "nine";
+                    break;
+            }
+
+            s = s.replaceAll(source,String.valueOf(i));
+        }
+
+        int answer = Integer.valueOf(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int answer = solution.solution("1zerotwozero3");
+        System.out.println(answer);
+
+    }
+}
